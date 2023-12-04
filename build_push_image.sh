9893410e7bc27c6e3b40451495c091134188f687
Jati Putra <h4t4k4@gmail.com>
2023-12-04 20:24:11 +0700

    Prepare to submit Submission

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+# Perintah untuk membuat Docker image dari Dockerfile yang tadi dibuat, dengan nama image item-app, dan memiliki tag v1.
 docker build -t item-app:v1 .
+# Melihat daftar image di lokal.
 docker images
+
+# Mengubah nama image agar sesuai dengan format Docker Hub.
+# docker tag item-app:v1 h4t4/item-app:v1
+
+# Mengubah nama image agar sesuai dengan format GitHub Packages .
 docker tag item-app:v1 ghcr.io/h4t4k4/item-app:v1
+
+
+# Login ke Docker Hub 
 # echo $PASSWORD_DOCKER_HUB | docker login -u h4t4 --password-stdin
+
+# Login ke GitHub Package
 echo $PAT | docker login ghcr.io -u h4t4k4 --password-stdin
+
+# Mengunggah image ke Docker Hub. 
+# docker push h4t4/item-app:v1
+
+# Mengunggah image ke GitHub Packages.
 docker push ghcr.io/h4t4k4/item-app:v1
