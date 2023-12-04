342e79ddddb5a95e661eeceb0f4ce87d279a26af
Jati Putra <h4t4k4@gmail.com>
2023-12-04 17:30:10 +0700

    modified to Github Package

--- build_push_image.sh
+++ build_push_image.sh
@@ -2,6 +2,7 @@
 
 docker build -t item-app:v1 .
 docker images
-docker tag item-app:v1 h4t4/item-app:v1
-echo $PASSWORD_DOCKER_HUB | docker login -u h4t4 --password-stdin
-docker push h4t4/item-app:v1
+docker tag item-app:v1 ghcr.io/h4t4k4/item-app:v1
+# echo $PASSWORD_DOCKER_HUB | docker login -u h4t4 --password-stdin
+echo $PAT | docker login ghcr.io -u h4t4k4 --password-stdin
+docker push ghcr.io/h4t4k4/item-app:v1
