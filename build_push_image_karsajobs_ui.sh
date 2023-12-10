7b9e6c5304aeab650d3beedd4234f7c7e16abb90
Jati Putra <h4t4k4@gmail.com>
2023-12-10 19:17:21 +0700

    After build, push and run on K8S

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-docker build -t h4t4/karsajobs-ui:latest .
-echo $PASSWORD_DOCKER_HUB | docker login -u h4t4 --password-stdin
-docker push h4t4/karsajobs-ui:latest
+# Build Docker image dari Dockerfile
+docker build -t ghcr.io/h4t4k4/karsajobs-ui:latest .
+
+# login ke docker hub
+# echo $PASSWORD_DOCKER_HUB | docker login -u h4t4 --password-stdin
+
+# login ke github package
+echo $PASSWORD_GITHUB_PACKAGE | docker login ghcr.io -u h4t4k4 --password-stdin
+
+# push image ke Dockerhub
+# docker push h4t4/karsajobs-ui:latest
+
+# push image ke Github package
+docker push ghcr.io/h4t4k4/karsajobs-ui:latest
