b0fb10b6a82b4356049057a12fadf7d20b01c126
Jati Putra <h4t4k4@gmail.com>
2023-12-16 05:53:11 +0700

    Add Dockerfile and sh file

---
+++ build_push_image_shipping_service.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Build Docker image dari Dockerfile
+docker build -t ghcr.io/h4t4k4/shipping-service:latest .
+
+docker build -t h4t4/shipping-service:latest .
+
+# login ke docker hub
+echo $PASSWORD_DOCKER_HUB | docker login -u h4t4 --password-stdin
+
+# push image ke Dockerhub
+docker push h4t4/shipping-service:latest
+
+# login ke github package
+echo $PASSWORD_GITHUB_PACKAGE | docker login ghcr.io -u h4t4k4 --password-stdin
+
+# push image ke Github package
+docker push ghcr.io/h4t4k4/shipping-service:latest
